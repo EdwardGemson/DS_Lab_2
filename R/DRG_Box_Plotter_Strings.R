9b9a60f54d6e92a1b4015a076cb286c428912298
edwardgemson <edwardgemson@gmail.com>
2019-10-23 16:55:23 -0400

    attempt to add string file

--- R/DRG_Box_Plotter_Strings.R
+++ R/DRG_Box_Plotter_Strings.R
@@ -3,28 +3,15 @@ library(ggplot2)
 library(mdsr)
 library(devtools)
 library(roxygen2)
-
 drg.data <- read.csv('data/DRG_data.csv')
-##create a variable for the DRG code
-#' Title
-#'
-#' @param df
-#' @param payment
-#'
-#' @return g
-#' @export
-#'
-#' @examples
-#' @importFrom ggplot2 ggplot
 drg_boxplot <- function(df, payment) {
   ##plot the average medicare payments
-  View(drg.data)
   df <- df %>%
     group_by(DRG.Definition) %>%
     mutate(DRG = strsplit(as.character(DRG.Definition),
-    split = ' ')[[1]][1]) %>%
+                          split = ' ')[[1]][1]) %>%
     ungroup()
-  g <- ggplot(df, aes(x = DRG.Definition,
+  g <- ggplot(df, aes_string(x = DRG,
                       y = payment)) +
     scale_y_continuous(trans='log10') +
     geom_boxplot(outlier.shape = NA) +
@@ -36,7 +23,4 @@ drg_boxplot <- function(df, payment) {
     ggtitle('Average Payments for Hospitals by DRG Code')
   g
 }
-
-drg_boxplot(drg.data, drg.data$Average.Medicare.Payments)
-
-
+drg_boxplot(drg.data, "Average.Medicare.Payments")
